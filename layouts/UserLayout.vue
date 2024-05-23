9e172713bf2d6e4d0c9c8352e2c92d166bd1889a
648lsp666 <815530704@qq.com>
2024-05-23 19:35:16 +0800

    frontend initialization with login and register

---
+++ layouts/UserLayout.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="height: 400px">
+      <a-layout-header class="header">
+        <MenuBar></MenuBar>
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        Written by
+        <a href="https://github.com/648lsp666" target="_blank" class="herf">
+          ThreeLi
+        </a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<style scoped>
+#basicLayout {
+}
+#basicLayout .header {
+}
+#basicLayout .content {
+  background: #42b983;
+}
+#basicLayout .footer {
+  background: linear-gradient(to bottom, #fffffe, aliceblue);
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+  padding: 12px;
+}
+</style>
+<script setup lang="ts">
+import MenuBar from "@/components/MenuBar.vue";
+</script>
---
+++ src/views/User/RegisterView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="login-view">登录页面</div>
+</template>
+
+<style scoped></style>
+<script setup lang="ts"></script>
